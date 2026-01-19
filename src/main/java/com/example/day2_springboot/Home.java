bc6edd359200faec5b38d7d8a8c05e6c57d8b0a2
Gun-Saxena <gunsaxena2004@gmail.com>
2026-01-19 14:53:06 +0530

    Initial commit

---
+++ src/main/java/com/example/day2_springboot/Home.java
@@ -0,0 +1,28 @@
+package com.example.day2_springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Home {
+    @GetMapping("/")
+    public String hello() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
